8f541a72924c3f7b00044ca3c80f9ea76501f03a
Leonard Clark <lclark@2bdx.net>
2012-07-07 00:21:03 -0700

    Applied Apache 2.0 license to source files in keeping with original project.

--- src/RingBuffer.hpp
+++ src/RingBuffer.hpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Leonard Clark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef __DISRUPTOR_RINGBUFFER_HPP__
 #define __DISRUPTOR_RINGBUFFER_HPP__
 
@@ -10,6 +25,14 @@
 
 namespace disruptor {
 
+/**
+ * Ring based store of reusable entries containing the data
+ * representing an event being exchanged between event publisher and
+ * {@link EventProcessor}s.
+ *
+ * @param <T> implementation storing the data for sharing during
+ * exchange or parallel coordination of an event.
+ */
 template <typename T>
 class RingBuffer
     : public Sequencer
